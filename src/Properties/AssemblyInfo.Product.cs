0bfdb8e06730289d45e645d5ad71f5856f372e5e
Kerry Jiang <kerry-jiang@hotmail.com>
2016-08-04 16:48:26 +0800

    added assemblyinfo file

---
+++ src/Properties/AssemblyInfo.Product.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.7.0.8")]
+[assembly: AssemblyFileVersion("1.7.0.8")]
+[assembly: AssemblyCompany("SuperSocket")]
+[assembly: AssemblyProduct("SuperSocket")]
+[assembly: AssemblyCopyright("Copyright © supersocket.codeplex.com 2010-2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
