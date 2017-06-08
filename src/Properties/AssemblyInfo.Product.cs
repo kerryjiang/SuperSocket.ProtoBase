06484bd13406b786abf7aa02d54ca052a222ae51
Kerry Jiang <kerry-jiang@hotmail.com>
2017-06-07 18:49:17 -0700

    1) added more runtime frameworks support for the nuget package; 2) added xml document to the nuget package; 3) signed .net standard assembly;

--- src/Properties/AssemblyInfo.Product.cs
+++ src/Properties/AssemblyInfo.Product.cs
@@ -2,8 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.7.0.16")]
-[assembly: AssemblyFileVersion("1.7.0.16")]
+[assembly: AssemblyVersion("1.7.0.17")]
+[assembly: AssemblyFileVersion("1.7.0.17")]
 [assembly: AssemblyCompany("SuperSocket")]
 [assembly: AssemblyProduct("SuperSocket")]
 [assembly: AssemblyCopyright("Copyright © www.supersocket.net 2010-2017")]
